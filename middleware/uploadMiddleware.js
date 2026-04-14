b5d004b7993740fd416f1a2ec7d25ea80cfe10d8
wijaya_magang_egov <wijayaganda879@gmail.com>
2026-04-14 23:25:15 +0700

    fix server 1

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,6 +1,5 @@
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs');
 const { createClient } = require('@supabase/supabase-js');
 
 // =============================================
